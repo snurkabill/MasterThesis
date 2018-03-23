e8489c4e5d2235004b95f0fe1d1a9e4fc8707f00
snurkabill <snurkabill@gmail.com>
2018-03-23 08:01:20 +0100

    Fixed default step penalty

--- HallwayGame/src/main/java/vahy/environment/config/DefaultGameConfig.java
+++ HallwayGame/src/main/java/vahy/environment/config/DefaultGameConfig.java
@@ -3,7 +3,7 @@ package vahy.environment.config;
 public class DefaultGameConfig implements IGameConfig {
 
     private final double goalReward = 100;
-    private final double stepPenalty = -1;
+    private final double stepPenalty = 1;
     private final double trapProbability = 0.1;
     private final double noisyMoveProbability = 0.1;
 
