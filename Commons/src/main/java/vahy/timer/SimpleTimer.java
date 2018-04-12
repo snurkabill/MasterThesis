c24810729ad30a6373983a376c78299e3f08177a
snurkabill <snurkabill@gmail.com>
2018-04-12 21:34:31 +0200

    Added simple timer

---
+++ Commons/src/main/java/vahy/timer/SimpleTimer.java
@@ -0,0 +1,34 @@
+package vahy.timer;
+
+public class SimpleTimer {
+
+    private long startingTime;
+    private long totalTime;
+    private boolean isRunning;
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void startTimer() {
+        startingTime = System.currentTimeMillis();
+        isRunning = true;
+    }
+
+    public void stopTimer() {
+        totalTime = System.currentTimeMillis() - startingTime;
+        isRunning = false;
+    }
+
+    public long getTotalTimeInMillis() {
+        return totalTime;
+    }
+
+    public double secondsSpent() {
+        return totalTime / 1000.0;
+    }
+
+    public double samplesPerSec(int samples) {
+        return (double) samples / (getTotalTimeInMillis() / 1000.0);
+    }
+}
