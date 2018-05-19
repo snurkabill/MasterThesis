644dd0f63b49cd6ce55bf76900e53b3a27c40ce3
snurkabill <snurkabill@gmail.com>
2018-05-19 18:14:53 +0200

    Added givenprobabilitiesUpdater to abstractNodeMetadata, cleared prototype file

--- HallwayGame/src/main/java/vahy/Prototype.java
+++ HallwayGame/src/main/java/vahy/Prototype.java
@@ -5,12 +5,15 @@ import org.slf4j.LoggerFactory;
 import vahy.api.episode.EpisodeAggregator;
 import vahy.api.model.State;
 import vahy.api.model.reward.RewardAggregator;
+import vahy.api.policy.Policy;
 import vahy.api.search.simulation.NodeEvaluationSimulator;
 import vahy.api.search.update.NodeTransitionUpdater;
 import vahy.chart.ChartBuilder;
 import vahy.environment.ActionType;
 import vahy.environment.agent.policy.environment.EnvironmentPolicy;
 import vahy.environment.agent.policy.exhaustive.BfsPolicy;
+import vahy.environment.agent.policy.randomized.EGreedyPolicy;
+import vahy.environment.agent.policy.smart.Ucb1Policy;
 import vahy.environment.config.ConfigBuilder;
 import vahy.environment.config.GameConfig;
 import vahy.environment.state.ImmutableStateImpl;
@@ -20,10 +23,15 @@ import vahy.impl.episode.EpisodeAggregatorImpl;
 import vahy.impl.model.DoubleVectorialObservation;
 import vahy.impl.model.reward.DoubleScalarReward;
 import vahy.impl.model.reward.DoubleScalarRewardAggregator;
+import vahy.impl.policy.random.UniformRandomWalkPolicy;
 import vahy.impl.search.node.nodeMetadata.AbstractSearchNodeMetadata;
 import vahy.impl.search.node.nodeMetadata.AbstractStateActionMetadata;
+import vahy.impl.search.node.nodeMetadata.ucb1.Ucb1SearchNodeMetadata;
+import vahy.impl.search.node.nodeMetadata.ucb1.Ucb1StateActionMetadata;
 import vahy.impl.search.simulation.MonteCarloSimulator;
 import vahy.impl.search.update.UniformAverageDiscountEstimateRewardTransitionUpdater;
+import vahy.search.AbstractMetadataWithGivenProbabilitiesTransitionUpdater;
+import vahy.search.Ucb1WithGivenProbabilitiesTransitionUpdater;
 import vahy.utils.ImmutableTuple;
 
 import java.io.File;
@@ -35,6 +43,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.SplittableRandom;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Prototype {
@@ -42,6 +51,144 @@ public class Prototype {
     private static final Logger logger = LoggerFactory.getLogger(Prototype.class);
 
     public static void main(String[] args) throws IOException, NotValidGameStringRepresentationException {
+        SplittableRandom random = new SplittableRandom(2);
+        GameConfig gameConfig = new ConfigBuilder().reward(1000).noisyMoveProbability(0.1).stepPenalty(1).trapProbability(1).buildConfig();
+        HallwayGameInitialInstanceSupplier hallwayGameInitialInstanceSupplier = getHallwayGameInitialInstanceSupplier(random, gameConfig);
+
+        RewardAggregator<DoubleScalarReward> rewardAggregator = new DoubleScalarRewardAggregator();
+        double discountFactor = 1;
+        int uniqueEpisodeCount = 1;
+        int episodeCount = 20;
+        int monteCarloSimulationCount = 100;
+        int updateTreeCount = 10000;
+        int totalEpisodes = uniqueEpisodeCount * episodeCount;
+
+        EpisodeAggregator<DoubleScalarReward> episodeAggregator = new EpisodeAggregatorImpl<>(
+            uniqueEpisodeCount,
+            episodeCount,
+            hallwayGameInitialInstanceSupplier,
+
+//            provideRandomWalkPolicy(random),
+//            provideBfsPolicy(random, rewardAggregator, discountFactor, monteCarloSimulationCount, updateTreeCount),
+            provideEGreedyPolicy(random, rewardAggregator, discountFactor, monteCarloSimulationCount, updateTreeCount),
+//            provideUcb1Policy(random, rewardAggregator, discountFactor, monteCarloSimulationCount, updateTreeCount),
+
+            new EnvironmentPolicy(random)
+            );
+
+        List<List<Double>> rewardHistory = episodeAggregator.runSimulation().stream().map(x -> x.stream().map(DoubleScalarReward::getValue).collect(Collectors.toList())).collect(Collectors.toList());
+        printChart(rewardHistory);
+        logger.info("Total reward: [{}]", rewardHistory.stream().map(x -> x.stream().reduce((aDouble, aDouble2) -> aDouble + aDouble2).get()).reduce((aDouble, aDouble2) -> aDouble + aDouble2).get());
+        logger.info("Average reward: [{}]", rewardHistory.stream().map(x -> x.stream().reduce((aDouble, aDouble2) -> aDouble + aDouble2).get()).reduce((aDouble, aDouble2) -> aDouble + aDouble2).get() / totalEpisodes);
+    }
+
+    public static Function<
+        State<ActionType, DoubleScalarReward, DoubleVectorialObservation>,
+        ImmutableTuple<Policy<ActionType, DoubleScalarReward, DoubleVectorialObservation>,
+            State<ActionType, DoubleScalarReward, DoubleVectorialObservation>>> provideRandomWalkPolicy(SplittableRandom random) {
+        return immutableState -> new ImmutableTuple<>(
+            new UniformRandomWalkPolicy<>(random),
+            immutableState);
+    }
+
+    public static Function<State<ActionType, DoubleScalarReward, DoubleVectorialObservation>, ImmutableTuple<Policy<ActionType, DoubleScalarReward, DoubleVectorialObservation>, State<ActionType, DoubleScalarReward, DoubleVectorialObservation>>> provideBfsPolicy(SplittableRandom random, RewardAggregator<DoubleScalarReward> rewardAggregator, double discountFactor, int monteCarloSimulationCount, int uprateTreeCount) {
+        return immutableState -> new ImmutableTuple<>(
+            new BfsPolicy(
+                random,
+                uprateTreeCount,
+                (ImmutableStateImpl) immutableState,
+                provideNodeTransitionUpdaterForAbstractMetadata(discountFactor, rewardAggregator),
+//                new CumulativeRewardSimulator<>()
+                provideMcEstimatorForAbstractMetadata(monteCarloSimulationCount, discountFactor, random, rewardAggregator)),
+            immutableState);
+    }
+
+    public static Function<State<ActionType, DoubleScalarReward, DoubleVectorialObservation>, ImmutableTuple<Policy<ActionType, DoubleScalarReward, DoubleVectorialObservation>, State<ActionType, DoubleScalarReward, DoubleVectorialObservation>>> provideUcb1Policy(SplittableRandom random, RewardAggregator<DoubleScalarReward> rewardAggregator, double discountFactor, int monteCarloSimulationCount, int uprateTreeCount) {
+        return immutableState -> new ImmutableTuple<>(
+            new Ucb1Policy(
+                random,
+                uprateTreeCount,
+                (ImmutableStateImpl) immutableState,
+//                provideNodeTransitionUpdaterForUcb1Metadata(discountFactor, rewardAggregator),
+                provideNodeTransitionUpdaterForUcb1WithGivenProbabilities(discountFactor, rewardAggregator),
+                provideMcEstimatorForUcb1Metadata(monteCarloSimulationCount, discountFactor, random, rewardAggregator)),
+            immutableState);
+    }
+
+    public static Function<State<ActionType, DoubleScalarReward, DoubleVectorialObservation>, ImmutableTuple<Policy<ActionType, DoubleScalarReward, DoubleVectorialObservation>, State<ActionType, DoubleScalarReward, DoubleVectorialObservation>>> provideEGreedyPolicy(SplittableRandom random, RewardAggregator<DoubleScalarReward> rewardAggregator, double discountFactor, int monteCarloSimulationCount, int uprateTreeCount) {
+        return immutableState -> new ImmutableTuple<>(
+            new EGreedyPolicy(
+                0.3,
+                random,
+                uprateTreeCount,
+                (ImmutableStateImpl) immutableState,
+//                provideNodeTransitionUpdaterForAbstractMetadata(discountFactor, rewardAggregator),
+                provideNodeTransitionUpdaterForAbstractMetadataWithGivenProbabilities(discountFactor, rewardAggregator),
+                provideMcEstimatorForAbstractMetadata(monteCarloSimulationCount, discountFactor, random, rewardAggregator)),
+            immutableState);
+    }
+
+    public static NodeTransitionUpdater<
+        ActionType,
+        DoubleScalarReward,
+        DoubleVectorialObservation,
+        AbstractStateActionMetadata<DoubleScalarReward>,
+        AbstractSearchNodeMetadata<ActionType, DoubleScalarReward, AbstractStateActionMetadata<DoubleScalarReward>>,
+        State<ActionType, DoubleScalarReward, DoubleVectorialObservation>> provideNodeTransitionUpdaterForAbstractMetadata(double discountFactor, RewardAggregator<DoubleScalarReward> rewardRewardAggregator) {
+        return new UniformAverageDiscountEstimateRewardTransitionUpdater<>(discountFactor, rewardRewardAggregator);
+    }
+
+    public static NodeTransitionUpdater<
+        ActionType,
+        DoubleScalarReward,
+        DoubleVectorialObservation,
+        Ucb1StateActionMetadata<DoubleScalarReward>,
+        Ucb1SearchNodeMetadata<ActionType, DoubleScalarReward>,
+        State<ActionType, DoubleScalarReward, DoubleVectorialObservation>> provideNodeTransitionUpdaterForUcb1Metadata(double discountFactor, RewardAggregator<DoubleScalarReward> rewardRewardAggregator) {
+        return new UniformAverageDiscountEstimateRewardTransitionUpdater<>(discountFactor, rewardRewardAggregator);
+    }
+
+    public static NodeTransitionUpdater<
+        ActionType,
+        DoubleScalarReward,
+        DoubleVectorialObservation,
+        Ucb1StateActionMetadata<DoubleScalarReward>,
+        Ucb1SearchNodeMetadata<ActionType, DoubleScalarReward>,
+        State<ActionType, DoubleScalarReward, DoubleVectorialObservation>> provideNodeTransitionUpdaterForUcb1WithGivenProbabilities(double discountFactor, RewardAggregator<DoubleScalarReward> rewardRewardAggregator) {
+        return new Ucb1WithGivenProbabilitiesTransitionUpdater(discountFactor, rewardRewardAggregator);
+    }
+
+    public static NodeTransitionUpdater<
+        ActionType,
+        DoubleScalarReward,
+        DoubleVectorialObservation,
+        AbstractStateActionMetadata<DoubleScalarReward>,
+        AbstractSearchNodeMetadata<ActionType, DoubleScalarReward, AbstractStateActionMetadata<DoubleScalarReward>>,
+        State<ActionType, DoubleScalarReward, DoubleVectorialObservation>> provideNodeTransitionUpdaterForAbstractMetadataWithGivenProbabilities(double discountFactor, RewardAggregator<DoubleScalarReward> rewardRewardAggregator) {
+        return new AbstractMetadataWithGivenProbabilitiesTransitionUpdater(discountFactor, rewardRewardAggregator);
+    }
+
+    public static NodeEvaluationSimulator<
+        ActionType,
+        DoubleScalarReward,
+        DoubleVectorialObservation,
+        AbstractStateActionMetadata<DoubleScalarReward>,
+        AbstractSearchNodeMetadata<ActionType, DoubleScalarReward, AbstractStateActionMetadata<DoubleScalarReward>>,
+        State<ActionType, DoubleScalarReward, DoubleVectorialObservation>> provideMcEstimatorForAbstractMetadata(int simulationCount, double discountFactor, SplittableRandom random, RewardAggregator<DoubleScalarReward> rewardAggregator) {
+        return new MonteCarloSimulator<>(simulationCount, discountFactor, random, rewardAggregator);
+    }
+
+    public static NodeEvaluationSimulator<
+        ActionType,
+        DoubleScalarReward,
+        DoubleVectorialObservation,
+        Ucb1StateActionMetadata<DoubleScalarReward>,
+        Ucb1SearchNodeMetadata<ActionType, DoubleScalarReward>,
+        State<ActionType, DoubleScalarReward, DoubleVectorialObservation>> provideMcEstimatorForUcb1Metadata(int simulationCount, double discountFactor, SplittableRandom random, RewardAggregator<DoubleScalarReward> rewardAggregator) {
+        return new MonteCarloSimulator<>(simulationCount, discountFactor, random, rewardAggregator);
+    }
+
+    public static HallwayGameInitialInstanceSupplier getHallwayGameInitialInstanceSupplier(SplittableRandom random, GameConfig gameConfig) throws NotValidGameStringRepresentationException, IOException {
         ClassLoader classLoader = Prototype.class.getClassLoader();
 //        URL url = classLoader.getResource("examples/hallway_demo0.txt");
 //        URL url = classLoader.getResource("examples/hallway_demo2.txt");
@@ -53,95 +200,7 @@ public class Prototype {
 //        URL url = classLoader.getResource("examples/hallway1-traps.txt");
 
         File file = new File(url.getFile());
-        SplittableRandom random = new SplittableRandom(2);
-        GameConfig gameConfig = new ConfigBuilder().reward(1000).noisyMoveProbability(0.0).stepPenalty(1).trapProbability(1).buildConfig();
-        HallwayGameInitialInstanceSupplier hallwayGameInitialInstanceSupplier = new HallwayGameInitialInstanceSupplier(gameConfig, random, new String(Files.readAllBytes(Paths.get(file.getAbsolutePath()))));
-
-
-        RewardAggregator<DoubleScalarReward> rewardAggregator = new DoubleScalarRewardAggregator();
-        double discountFactor = 1;
-        int uniqueEpisodeCount = 1;
-        int episodeCount = 20;
-        int totalEpisodes = uniqueEpisodeCount * episodeCount;
-
-//        NodeTransitionUpdater<
-//            ActionType,
-//            DoubleScalarReward,
-//            DoubleVectorialObservation,
-//            Ucb1StateActionMetadata<DoubleScalarReward>,
-//            Ucb1SearchNodeMetadata<ActionType, DoubleScalarReward>,
-//            State<ActionType, DoubleScalarReward, DoubleVectorialObservation>> transitionUpdater = new Ucb1WithGivenProbabilitiesTransitionUpdater(discountFactor, rewardAggregator);
-//
-//        NodeEvaluationSimulator<
-//            ActionType,
-//            DoubleScalarReward,
-//            DoubleVectorialObservation,
-//            Ucb1StateActionMetadata<DoubleScalarReward>,
-//            Ucb1SearchNodeMetadata<ActionType, DoubleScalarReward>,
-//            State<ActionType, DoubleScalarReward, DoubleVectorialObservation>> rewardSimulator = new MonteCarloSimulator<>(1000, discountFactor, random, rewardAggregator);
-
-        NodeTransitionUpdater<
-            ActionType,
-            DoubleScalarReward,
-            DoubleVectorialObservation,
-            AbstractStateActionMetadata<DoubleScalarReward>,
-            AbstractSearchNodeMetadata<ActionType, DoubleScalarReward, AbstractStateActionMetadata<DoubleScalarReward>>,
-            State<ActionType, DoubleScalarReward, DoubleVectorialObservation>> transitionUpdater = new UniformAverageDiscountEstimateRewardTransitionUpdater<>(discountFactor, rewardAggregator);
-
-        NodeEvaluationSimulator<
-            ActionType,
-            DoubleScalarReward,
-            DoubleVectorialObservation,
-            AbstractStateActionMetadata<DoubleScalarReward>,
-            AbstractSearchNodeMetadata<ActionType, DoubleScalarReward, AbstractStateActionMetadata<DoubleScalarReward>>,
-            State<ActionType, DoubleScalarReward, DoubleVectorialObservation>> rewardSimulator = new MonteCarloSimulator<>(1000, discountFactor, random, rewardAggregator);
-
-        EpisodeAggregator<DoubleScalarReward> episodeAggregator = new EpisodeAggregatorImpl<>(
-            uniqueEpisodeCount,
-            episodeCount,
-            hallwayGameInitialInstanceSupplier,
-
-//            immutableState -> new ImmutableTuple<>(
-//                new UniformRandomWalkPolicy(random),
-//                immutableState),
-
-
-            immutableState -> new ImmutableTuple<>(
-                new BfsPolicy(
-                    random,
-                    200,
-                    (ImmutableStateImpl) immutableState,
-                    transitionUpdater,
-                    rewardSimulator),
-                immutableState),
-
-
-//            immutableState -> new ImmutableTuple<>(
-//                new EGreedyPolicy(
-//                    random,
-//                    1000,
-//                    0.3,
-//                    immutableState,
-//                    transitionUpdater,
-//                    rewardSimulator),
-//                immutableState),
-
-//            immutableState -> new ImmutableTuple<>(
-//                new Ucb1Policy(
-//                    random,
-//                    2000,
-//                    immutableState,
-//                    transitionUpdater,
-//                    rewardSimulator),
-//                immutableState),
-
-            new EnvironmentPolicy(random)
-            );
-
-        List<List<Double>> rewardHistory = episodeAggregator.runSimulation().stream().map(x -> x.stream().map(DoubleScalarReward::getValue).collect(Collectors.toList())).collect(Collectors.toList());
-        printChart(rewardHistory);
-        logger.info("Total reward: [{}]", rewardHistory.stream().map(x -> x.stream().reduce((aDouble, aDouble2) -> aDouble + aDouble2).get()).reduce((aDouble, aDouble2) -> aDouble + aDouble2).get());
-        logger.info("Average reward: [{}]", rewardHistory.stream().map(x -> x.stream().reduce((aDouble, aDouble2) -> aDouble + aDouble2).get()).reduce((aDouble, aDouble2) -> aDouble + aDouble2).get() / totalEpisodes);
+        return new HallwayGameInitialInstanceSupplier(gameConfig, random, new String(Files.readAllBytes(Paths.get(file.getAbsolutePath()))));
     }
 
     public static void printChart(List<List<Double>> rewardHistory) {
