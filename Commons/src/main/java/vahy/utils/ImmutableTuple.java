9b859c6f308c2e3a70451ea61456a7a287d91c30
snurkabill <snurkabill@gmail.com>
2018-06-24 02:10:21 +0200

    Added hash code to immutableTuple

--- Commons/src/main/java/vahy/utils/ImmutableTuple.java
+++ Commons/src/main/java/vahy/utils/ImmutableTuple.java
@@ -17,4 +17,26 @@ public class ImmutableTuple<T1, T2> {
     public T2 getSecond() {
         return second;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ImmutableTuple)) {
+            return false;
+        }
+        ImmutableTuple<?, ?> that = (ImmutableTuple<?, ?>) o;
+        if (getFirst() != null ? !getFirst().equals(that.getFirst()) : that.getFirst() != null) {
+            return false;
+        }
+        return getSecond() != null ? getSecond().equals(that.getSecond()) : that.getSecond() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getFirst() != null ? getFirst().hashCode() : 0;
+        result = 31 * result + (getSecond() != null ? getSecond().hashCode() : 0);
+        return result;
+    }
 }
