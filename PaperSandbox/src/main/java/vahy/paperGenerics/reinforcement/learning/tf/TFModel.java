7001f52901c2aa03834632f7c88dc687f643e5e0
snurkabill <snurkabill@gmail.com>
2019-07-14 03:55:04 +0200

    Faster TF model

--- PaperSandbox/src/main/java/vahy/paperGenerics/reinforcement/learning/tf/TFModel.java
+++ PaperSandbox/src/main/java/vahy/paperGenerics/reinforcement/learning/tf/TFModel.java
@@ -26,53 +26,52 @@ public class TFModel extends PaperModel implements SupervisedTrainableModel, Aut
     private final SplittableRandom random;
     private final Session sess;
     private final double[][] trainInputBatch;
-    private final double[] trainQTargetBatch;
-    private final double[] trainRTargetBatch;
-    private final double[][] trainPolicyTargetBatch;
+    private final double[][] trainTargetBatch;
     private final SimpleTimer timer = new SimpleTimer();
 
-    private final boolean omitProbabilities;
+    private double[] outputVector;
+    private DoubleBuffer doubleBuffer;
+    private double[][] inputMatrixForOneVector;
+    private Tensor<Double> inferenceKeepProbability = Tensors.create(1.0);
 
-    public TFModel(int inputDimension, int outputDimension, int trainingIterations, int batchSize, byte[] bytes, SplittableRandom random, boolean omitProbabilities) {
+    public TFModel(int inputDimension, int outputDimension, int trainingIterations, int batchSize, byte[] bytes, SplittableRandom random) {
         this.inputDimension = inputDimension;
         this.outputDimension = outputDimension;
         this.trainingIterations = trainingIterations;
         this.batchSize = batchSize;
         this.random = random;
-        this.omitProbabilities = omitProbabilities;
-        trainInputBatch = new double[batchSize][];
-        trainPolicyTargetBatch = new double[batchSize][];
-        trainQTargetBatch = new double[batchSize];
-        trainRTargetBatch = new double[batchSize];
+        this.trainInputBatch = new double[batchSize][];
+        this.trainTargetBatch = new double[batchSize][];
         for (int i = 0; i < batchSize; i++) {
             trainInputBatch[i] = new double[inputDimension];
-            trainPolicyTargetBatch[i] = new double[outputDimension - POLICY_START_INDEX];
+            trainTargetBatch[i] = new double[outputDimension];
         }
+
+        this.outputVector = new double[outputDimension];
+        this.doubleBuffer = DoubleBuffer.wrap(outputVector);
+        this.inputMatrixForOneVector = new double[1][inputDimension];
+
         Graph graph = new Graph();
         this.sess = new Session(graph);
         graph.importGraphDef(bytes);
         this.sess.runner().addTarget("init").run();
 
-//        this.sess = savedModelBundle.session();
         logger.info("Initialized model based on TensorFlow backend.");
         logger.debug("Model with input dimension: [{}] and output dimension: [{}]. Batch size of model set to: [{}]", inputDimension, outputDimension, batchSize);
     }
 
-    private void fillbatch(int batchesDone, int[] order, double[][] input, double[][] target) {
-        logger.trace("Filling data batch. Already done: [{}]", batchesDone);
+    private void fillBatch(int batchesDone, int[] order, double[][] input, double[][] target) {
+        logger.trace("Filling data batch. Already done batches: [{}]", batchesDone);
         for (int i = 0; i < batchSize; i++) {
             int index = batchesDone * batchSize + i;
             if(index >= order.length) {
                 break; // leaving part of batch from previous iteration.
             }
             System.arraycopy(input[order[index]], 0, trainInputBatch[i], 0, inputDimension);
-            System.arraycopy(target[order[index]], POLICY_START_INDEX, trainPolicyTargetBatch[i], 0, outputDimension - POLICY_START_INDEX);
-            trainQTargetBatch[i] = target[order[index]][Q_VALUE_INDEX];
-            trainRTargetBatch[i] = target[order[index]][RISK_VALUE_INDEX];
+            System.arraycopy(target[order[index]], 0, trainTargetBatch[i], 0, outputDimension);
         }
     }
 
-    @Override
     public void fit(double[][] input, double[][] target) {
         if(input.length != target.length) {
             throw new IllegalArgumentException("Input and target lengths differ");
@@ -83,20 +82,16 @@ public class TFModel extends PaperModel implements SupervisedTrainableModel, Aut
         for (int i = 0; i < trainingIterations; i++) {
             shuffleArray(order, new Random(random.nextInt()));
             for (int j = 0; j < (target.length / batchSize) + 1; j++) {
-                fillbatch(j, order, input, target);
+                fillBatch(j, order, input, target);
                 try (
                     Tensor<Double> tfInput = Tensors.create(this.trainInputBatch);
-                    Tensor<Double> tfQTarget = Tensors.create(this.trainQTargetBatch);
-                    Tensor<Double> tfRTarget = Tensors.create(this.trainRTargetBatch);
-                    Tensor<Double> tfPolicyTarget = Tensors.create(this.trainPolicyTargetBatch);
-                    ) {
+                    Tensor<Double> tfTarget = Tensors.create(this.trainTargetBatch);
+                ) {
                     sess
                         .runner()
                         .feed("input_node", tfInput)
-                        .feed("Q_target_node", tfQTarget)
-                        .feed("Risk_target_node", tfRTarget)
-                        .feed("Policy_target_node", tfPolicyTarget)
-                        .addTarget("train_node")
+                        .feed("target_node", tfTarget)
+                        .addTarget("optimize_node")
                         .run();
                 }
             }
@@ -105,6 +100,7 @@ public class TFModel extends PaperModel implements SupervisedTrainableModel, Aut
         logger.debug("Training of [{}] inputs with minibatch size [{}] took [{}] milliseconds. Samples per sec: [{}]",
             input.length, batchSize, timer.getTotalTimeInMillis(), timer.samplesPerSec(input.length));
     }
+
 
     private static void shuffleArray(int[] array, Random rng) {
         for(int i = array.length - 1; i > 0; --i) {
@@ -115,64 +111,49 @@ public class TFModel extends PaperModel implements SupervisedTrainableModel, Aut
         }
     }
 
-    @Override
     public double[] predict(double[] input) {
-        double[][] matrix = new double[1][];
-        matrix[0] = input;
-        try (Tensor<Double> tfInput = Tensors.create(matrix)) {
-            Tensor<Double> output = sess
+        System.arraycopy(input, 0, inputMatrixForOneVector[0], 0, inputDimension);
+        try (Tensor<Double> tfInput = Tensors.create(inputMatrixForOneVector)) {
+            Tensor<?> output = sess
                 .runner()
                 .feed("input_node", tfInput)
-                .fetch("prediction_node_2")
+                .fetch("prediction_node")
                 .run()
-                .get(0)
-                .expect(Double.class);
-            double[] outputVector = new double[outputDimension];
-            DoubleBuffer doubleBuffer = DoubleBuffer.wrap(outputVector);
+                .get(0);
+
             output.writeTo(doubleBuffer);
+            doubleBuffer.position(0);
             output.close();  // needed?
-            if(omitProbabilities) {
-                for (int i = 0; i < inputDimension - 2; i++) {
-                    outputVector[i + 2] = 1.0 / (inputDimension - 2);
-                }
-                return outputVector;
-            } else {
-                return outputVector;
-            }
+            return outputVector;
         }
     }
 
-    @Override
     public double[][] predict(double[][] input) {
-        throw new UnsupportedOperationException();
-//        try (Tensor<Double> tfInput = Tensors.create(input)) {
-//            List<Tensor<Double>> output = sess
-//                .runner()
-//                .feed("input_node", tfInput)
-//                .fetch("prediction_node_2")
-//                .run()
-//                .stream()
-//                .map(x -> x.expect(Double.class))
-//                .collect(Collectors.toList());
-//
-//            double[] outputBuffer = new double[outputDimension * inputDimension];
-//            DoubleBuffer doubleBuffer = DoubleBuffer.wrap(outputBuffer);
-//            double[][] outputMatrix = new double[input.length][];
-//            for (int i = 0; i < outputMatrix.length; i++) {
-//                outputMatrix[i] = new double[outputDimension];
-//                System.arraycopy(outputBuffer, i * outputDimension, outputMatrix[i], 0, outputDimension);
-//            }
-//            output.forEach(Tensor::close); // needed?
-//            return outputMatrix;
-//        }
+        try (Tensor<Double> tfInput = Tensors.create(input)) {
+            double[] outputBuffer = new double[outputDimension * input.length];
+            DoubleBuffer doubleBuffer = DoubleBuffer.wrap(outputBuffer);
+            sess
+                .runner()
+                .feed("input_node", tfInput)
+                .fetch("prediction_node")
+                .run()
+                .forEach(x -> {
+                    x.writeTo(doubleBuffer);
+                    x.close(); // needed?
+                });
+            double[][] outputMatrix = new double[input.length][];
+            for (int i = 0; i < outputMatrix.length; i++) {
+                outputMatrix[i] = new double[outputDimension];
+                System.arraycopy(outputBuffer, i * outputDimension, outputMatrix[i], 0, outputDimension);
+            }
+            return outputMatrix;
+        }
     }
 
-    @Override
     public int getInputDimension() {
         return inputDimension;
     }
 
-    @Override
     public int getOutputDimension() {
         return outputDimension;
     }
@@ -181,6 +162,7 @@ public class TFModel extends PaperModel implements SupervisedTrainableModel, Aut
     public void close() {
         logger.trace("Finalizing TF model resources");
         sess.close();
+        inferenceKeepProbability.close();
         logger.debug("TF resources closed");
     }
 }
--- PaperSandbox/src/main/java/vahy/paperGenerics/reinforcement/learning/tf/create_model.py
+++ PaperSandbox/src/main/java/vahy/paperGenerics/reinforcement/learning/tf/create_model.py
@@ -1,118 +1,58 @@
 import tensorflow as tf
 
-# input_count = 1
-
-benchmark_name = 'randomWalk'
-input_count = 2
- # input_count = 34
-
+benchmark_name = 'BENCHMARK_05'
+input_count = 9
 hidden_count_1 = 10
-hidden_count_2 = 100
+hidden_count_2 = 10
 hidden_count_3 = 10
+Q_output_count = 1
+risk_output_count = 1
+action_output_count = 3
 
-action_count = 2
-# action_count = 5
+output_count = Q_output_count + risk_output_count + action_output_count
 
 tf.reset_default_graph()
-
 
 Relu = tf.nn.relu
 Tanh = tf.nn.tanh
 BatchNormalization = tf.layers.batch_normalization
-Dropout = tf.layers.dropout
 Dense = tf.layers.dense
+Dropout = tf.nn.dropout
 
 
+x = tf.placeholder(tf.float64, [None, input_count], name= 'input_node')
+target = tf.placeholder(tf.float64, [None, output_count], name = "target_node")
 
-x = tf.placeholder(tf.double, [None, input_count], name= 'input_node')
-q_target = tf.placeholder(tf.double, [None, 1], name = 'Q_target_node')
-risk_target = tf.placeholder(tf.double, [None, 1], name = 'Risk_target_node')
-policy_target = tf.placeholder(tf.double, [None, action_count], name = 'Policy_target_node')
+Q_target = tf.slice(target, [0, 0], [-1, Q_output_count], name = "Q_slice_node")
+risk_target = tf.slice(target, [0, Q_output_count], [-1, risk_output_count], name = "risk_slice_node")
+action_target = tf.slice(target, [0, Q_output_count + risk_output_count], [-1, action_output_count], name = "action_slice_node")
 
-hidden_1 = Dropout(Dense(x,        hidden_count_1, tf.nn.relu, True, tf.glorot_normal_initializer(), name = "Hidden_1")) #, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=0.0))
-# hidden_2 = Dropout(Dense(hidden_1, hidden_count_2, tf.nn.relu, True, tf.glorot_normal_initializer(), name = "Hidden_2")) #, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=0.0))
-# hidden_3 = Dense(hidden_2, hidden_count_3, tf.nn.relu, True, tf.glorot_normal_initializer(), name = "Hidden_3") #, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=0.0))
+# hidden_1 = Dense(x,        hidden_count_1, tf.nn.relu, use_bias = True, kernel_initializer = tf.glorot_normal_initializer(), name = "hidden_1") #, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=0.0))
+# hidden_2 = Dense(hidden_1, hidden_count_2, tf.nn.relu, use_bias = True, kernel_initializer = tf.glorot_normal_initializer(), name = "hidden_2") #, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=0.0))
+# hidden_3 = Dense(hidden_2, hidden_count_3, tf.nn.relu, use_bias = True, kernel_initializer = tf.glorot_normal_initializer(), name = "hidden_3") #, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=0.0))
 
-policy = tf.layers.dense(hidden_1, action_count, tf.nn.softmax, use_bias = True, kernel_initializer = tf.zeros_initializer, bias_initializer = tf.zeros_initializer, name = 'policy_node')
-risk =   tf.layers.dense(hidden_1, 1,            tf.nn.sigmoid,    use_bias = True, kernel_initializer = tf.zeros_initializer, name = "risk_node")
-q =      tf.layers.dense(hidden_1, 1,                           use_bias = True, kernel_initializer = tf.zeros_initializer, name = "q_node")
+Q_output      = tf.layers.dense(x, Q_output_count,                   use_bias = True, kernel_initializer = tf.zeros_initializer, bias_initializer = tf.zeros_initializer, name = 'Q_output_node')
+risk_output   = tf.layers.dense(x, risk_output_count, tf.nn.relu, use_bias = True, kernel_initializer = tf.zeros_initializer, bias_initializer = tf.zeros_initializer, name = "risk_output_node")
+action_output = tf.layers.dense(x, action_output_count, tf.nn.softmax, use_bias = True, kernel_initializer = tf.zeros_initializer, bias_initializer = tf.zeros_initializer, name = "action_output_node")
 
-prediction = tf.concat([q, risk, policy], 1, name = "prediction_node_2")
+prediction = tf.concat([Q_output, risk_output, action_output], 1, name = "concat_node")
+prediction_identity = tf.identity(prediction, name = "prediction_node")
 
-# policy_loss = tf.reduce_mean(softmax_cross_entropy_with_logits_v2(logits = policy, labels = policy_target), name = 'policy_loss')
+Q_loss = tf.keras.losses.mean_squared_error(y_true = Q_target, y_pred = Q_output)
+risk_loss = tf.keras.losses.binary_crossentropy(y_true = risk_target, y_pred = risk_output)
+policy_loss = tf.keras.losses.categorical_crossentropy(y_true = action_target, y_pred = action_output)
 
-#policy_loss = tf.keras.losses.KLD(y_true = policy_target, y_pred = policy)
-policy_loss = tf.keras.losses.categorical_crossentropy(y_true = policy_target, y_pred = policy)
-
-q_loss = tf.keras.losses.mean_squared_error(y_true = q_target, y_pred = q)
-r_loss = tf.keras.losses.mean_squared_error(y_true = risk_target, y_pred = risk)
-
-# total_loss = policy_loss + q_loss + r_loss
-total_loss = q_loss + r_loss + policy_loss
-# total_loss = q_loss
-
-
-
-# total_loss = q_loss + r_loss
-# total_loss = r_loss
-# total_loss = q_loss
-# total_loss = policy_loss
-
-
-optimizer = tf.train.AdamOptimizer(learning_rate = 0.01, name = "Adam")
-train_op = optimizer.minimize(total_loss, name = 'train_node')
+total_loss = Q_loss + risk_loss + policy_loss
+train_op = tf.train.AdamOptimizer(learning_rate = 0.01, name = "Optimizer").minimize(total_loss, name = 'optimize_node')
 
 init = tf.global_variables_initializer()
 
-
 sess = tf.Session()
+
+train_writer = tf.summary.FileWriter('myGraph', sess.graph)
+train_writer.close()
+
 sess.run(init)
-
-
-
-
-
-
-print(q.name)
-
-# n_samples = 0
-# avg_cost = 0.
-# for epoch in range(100000):
-#     n_samples = n_samples + 1
-#
-#     # batch_xs = [[1], [2], [3], [4]]
-#
-#     batch_xs = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
-#
-#     batch_Q = [[96], [97], [98], [99]]
-#     batch_R = [[0.9], [0.6], [0.4], [0.1]]
-#     batch_Policy = [[0.02, 0.49, 0.49], [0.01, 0.01, 0.98], [0.01, 0.98, 0.01], [0.98, 0.01, 0.01]]
-#     policy_cost, q_cost, r_cost, total_cost, _ = sess.run((policy_loss, q_loss, r_loss, total_loss, train_op), feed_dict= {x: batch_xs, q_target: batch_Q, risk_target: batch_R, policy_target: batch_Policy})
-#
-#     predictedVector = sess.run(prediction, feed_dict= {x: batch_xs, q_target: batch_Q, risk_target: batch_R, policy_target: batch_Policy})
-#
-#     np.set_printoptions(suppress=True)
-#     print(predictedVector)
-#
-#     print("Epoch:", '%04d' % (epoch + 1), "total_cost = ", "{:.9f} ".format(sum(total_cost)), "q_cost = ", "{:.9f} ".format(sum(q_cost)), "r_cost = ", "{:.9f} ".format(sum(r_cost)), "policy_cost = ", "{:.9f} ".format(sum(policy_cost)))
-#
-#
-#
-#
-# saver_def = tf.trainPolicy.Saver().as_saver_def()
-#
-# print('Operation to initialize variables:       ', init.name)
-# print('Tensor to feed as input data:            ', x.name)
-# print('Tensor to feed as training targets:      ', y_.name)
-#
-#
-# print('Tensor to fetch as prediction:           ', y.name)
-# print('Operation to trainPolicy one step:             ', train_op.name)
-# print('Tensor to be fed for checkpoint filename:', saver_def.filename_tensor_name)
-# print('Operation to save a checkpoint:          ', saver_def.save_tensor_name)
-# print('Operation to restore a checkpoint:       ', saver_def.restore_op_name)
-# print('Tensor to read value of W                ', W.value().name)
-# print('Tensor to read value of b                ', b.value().name)
 
 with open('../../../../../../resources/tfModel/graph_' + benchmark_name + '.pb', 'wb') as f:
     f.write(tf.get_default_graph().as_graph_def().SerializeToString())
