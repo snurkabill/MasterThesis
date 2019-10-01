cd9bba24a0ca3edcd258c096e82d1bb4d5da5e6d
snurkabill <snurkabill@gmail.com>
2019-10-01 23:36:03 +0200

    Printing episode results

--- PaperSandbox/src/main/java/vahy/paperGenerics/reinforcement/episode/PolicyStepRecord.java
+++ PaperSandbox/src/main/java/vahy/paperGenerics/reinforcement/episode/PolicyStepRecord.java
@@ -1,5 +1,9 @@
 package vahy.paperGenerics.reinforcement.episode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PolicyStepRecord {
 
     private final double[] priorProbabilities;
@@ -28,5 +32,41 @@ public class PolicyStepRecord {
 
     public double getRisk() {
         return risk;
+    }
+
+    @Override
+    public String toString() {
+        return "PolicyStepRecord{" +
+                "priorProbabilities=" + Arrays.toString(priorProbabilities) +
+                ", policyProbabilities=" + Arrays.toString(policyProbabilities) +
+                ", rewardPredicted=" + rewardPredicted +
+                ", risk=" + risk +
+                '}';
+    }
+
+    public List<String> getCsvHeader() {
+        var list = new ArrayList<String>();
+        for (int i = 0; i < priorProbabilities.length; i++) {
+            list.add("PriorProbabilities_" + i);
+        }
+        for (int i = 0; i < policyProbabilities.length; i++) {
+            list.add("PolicyProbabilities_" + i);
+        }
+        list.add("Predicted reward");
+        list.add("Predicted risk");
+        return list;
+    }
+
+    public List<String> getCsvRecord() {
+        var list = new ArrayList<String>();
+        for (int i = 0; i < priorProbabilities.length; i++) {
+            list.add(Double.toString(priorProbabilities[i]));
+        }
+        for (int i = 0; i < policyProbabilities.length; i++) {
+            list.add(Double.toString(policyProbabilities[i]));
+        }
+        list.add(Double.toString(rewardPredicted));
+        list.add(Double.toString(risk));
+        return list;
     }
 }
