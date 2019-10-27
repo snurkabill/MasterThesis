7c3c275e5f21dd682eee6c7572b80852c3106733
snurkabill <snurkabill@gmail.com>
2019-10-27 20:04:36 +0100

    prepared api for childcontainer

---
+++ StateSpaceSearch/src/main/java/vahy/api/search/node/ChildNodeContainer.java
@@ -0,0 +1,21 @@
+package vahy.api.search.node;
+
+public interface ChildNodeContainer {
+
+
+    // entrySet<Action, SearchNode>()
+
+    // SearchNode get(Action)
+
+    // boolean containsKey(Action)
+
+    // List<SearchNode>/SearchNode[] values()
+
+    // int size()
+
+    // clear()
+
+    // put(Action, SearchNode)
+
+
+}
