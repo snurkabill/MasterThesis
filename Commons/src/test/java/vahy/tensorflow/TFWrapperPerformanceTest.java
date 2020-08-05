03d55e2d2a1c79929417a2933a9a0528da55459b
snurkabill <snurkabill@gmail.com>
2020-08-05 22:29:58 +0200

    TF throughput performance test

--- Commons/src/test/java/vahy/tensorflow/TFWrapperPerformanceTest.java
+++ Commons/src/test/java/vahy/tensorflow/TFWrapperPerformanceTest.java
@@ -34,7 +34,7 @@ public class TFWrapperPerformanceTest {
 
         try {
             var modelRepresentation = TFHelper.loadTensorFlowModel(modelPath, environmentPath, random.nextLong(), inputDim, outputDim, 0);
-            try(TFModelImproved model = new TFModelImproved(inputDim, outputDim, batchSize, trainingIterations_IN_MODEL, 0.5, 0.0001, modelRepresentation, 1, random))
+            try(TFModelImproved model = new TFModelImproved(inputDim, outputDim, batchSize, trainingIterations_IN_MODEL, 0.7, 0.0001, modelRepresentation, 1, random))
             {
                 for (int i = 0; i < trainingIterations_OUTSIDE_MODEL; i++) {
                     trainingLoop(inputTrainData, targetTrainData, model);
@@ -65,6 +65,70 @@ public class TFWrapperPerformanceTest {
                 }
                 long end = System.currentTimeMillis();
                 logger.info("Time per prediction in us: " + ((end - start) * 1000) / (double) (instanceCount * trials));
+            }
+        } catch (Exception e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    public void throughputTest() {
+        SplittableRandom random = new SplittableRandom(0);
+
+        int inputDim = 512;
+        int outputDim = 512;
+
+        int instanceCount = 100;
+
+        double[][] inputData = new double[instanceCount][];
+        double[][] targetData = new double[instanceCount][];
+
+        for (int i = 0; i < instanceCount; i++) {
+            double[] input = new double[inputDim];
+            double[] target = new double[outputDim];
+            for (int j = 0; j < inputDim; j++) {
+                input[j] = random.nextDouble() - 0.5;
+            }
+            for (int j = 0; j < outputDim; j++) {
+                target[j] = random.nextDouble();
+            }
+            inputData[i] = input;
+            targetData[i] = target;
+        }
+
+        int trainingIterations_IN_MODEL = 1;
+        int trainingIterations_OUTSIDE_MODEL = 1;
+        int batchSize = 1;
+
+//        String environmentPath = System.getProperty("user.home") + "/.local/virtualenvs/tf_2_3/bin/python";
+        String environmentPath = System.getProperty("user.home") + "/.local/virtualenvs/tensorflow_2_0/bin/python";
+        var modelPath = Paths.get(ConcurrencyTesting.class.getClassLoader().getResource("tfModelPrototypes/HUGE_MODEL.py").getPath());
+
+        try {
+            var modelRepresentation = TFHelper.loadTensorFlowModel(modelPath, environmentPath, random.nextLong(), inputDim, outputDim, 0);
+            try(TFModelImproved model = new TFModelImproved(inputDim, outputDim, batchSize, trainingIterations_IN_MODEL, 0.7, 0.0001, modelRepresentation, 1, random))
+            {
+                for (int i = 0; i < trainingIterations_OUTSIDE_MODEL; i++) {
+                    trainingLoop(inputData, targetData, model);
+                }
+
+                var trials = 100;
+                long start = System.currentTimeMillis();
+                for (int i = 0; i < trials; i++) {
+                    for (int j = 0; j < instanceCount; j++) {
+                        model.predict(inputData[j]);
+                    }
+                }
+                long end = System.currentTimeMillis();
+                logger.info("Time per single prediction in us: " + ((end - start) * 1000) / (double) (instanceCount * trials));
+
+                start = System.currentTimeMillis();
+                for (int i = 0; i < trials; i++) {
+                    model.predict(inputData);
+                }
+                end = System.currentTimeMillis();
+                logger.info("Time per batched prediction in us: " + ((end - start) * 1000) / (double) (instanceCount * trials));
+                logger.info("Time per prediction in batch per sample in us: " + (((end - start) * 1000) / (double) (instanceCount * trials)) / instanceCount);
             }
         } catch (Exception e) {
             Assertions.fail(e);
