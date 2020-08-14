73b456e97a90d4918fd9a435005784659cc348e7
snurkabill <snurkabill@gmail.com>
2020-08-14 11:49:45 +0200

    Merged cherrypick

--- Commons/src/test/resources/tfModelPrototypes/AND.py
+++ Commons/src/test/resources/tfModelPrototypes/AND.py
@@ -1,9 +1,9 @@
 import os
 
 import sys
-import tensorflow as tf
+import tensorflow._api.v2.compat.v1 as tf
 
-tf.compat.v1.disable_v2_behavior()
+tf.disable_v2_behavior()
 
 model_name = sys.argv[1]
 input_count = int(sys.argv[2])
@@ -14,37 +14,37 @@ seed = int(sys.argv[6])
 
 print("INITIALIZING TF MODEL WITH SEED " + str(seed))
 
-tf.compat.v1.reset_default_graph()
-tf.compat.v1.random.set_random_seed(seed)
+tf.reset_default_graph()
+tf.random.set_random_seed(seed)
 
-x = tf.compat.v1.placeholder(tf.compat.v1.float64, [None, 2], name= 'input_node')
-target = tf.compat.v1.placeholder(tf.compat.v1.float64, [None, 1], name = "target_node")
-keep_prob = tf.compat.v1.placeholder(tf.compat.v1.float64, [], name = "keep_prob_node")
-learning_rate = tf.compat.v1.placeholder(tf.compat.v1.float64, [], name = "learning_rate_node")
+x = tf.placeholder(tf.float64, [None, 2], name= 'input_node')
+target = tf.placeholder(tf.float64, [None, 1], name = "target_node")
+keep_prob = tf.placeholder(tf.float64, [], name = "keep_prob_node")
+learning_rate = tf.placeholder(tf.float64, [], name = "learning_rate_node")
 
-output = tf.compat.v1.layers.dense(x, 1, tf.nn.tanh, use_bias = True, kernel_initializer = tf.compat.v1.zeros_initializer, bias_initializer = tf.compat.v1.zeros_initializer, name = 'output_node')
-prediction = tf.compat.v1.identity(output, name = "prediction_node")
+output = tf.layers.dense(x, 1, tf.nn.tanh, use_bias = True, kernel_initializer = tf.zeros_initializer, bias_initializer = tf.zeros_initializer, name = 'output_node')
+prediction = tf.identity(output, name = "prediction_node")
 
-loss = tf.compat.v1.keras.losses.mean_squared_error(y_true = target, y_pred = output)
-train_op = tf.compat.v1.train.AdamOptimizer(learning_rate = 0.01, name = "Optimizer").minimize(loss, name ='optimize_node')
+loss = tf.keras.losses.mean_squared_error(y_true = target, y_pred = output)
+train_op = tf.train.AdamOptimizer(learning_rate = 0.01, name = "Optimizer").minimize(loss, name ='optimize_node')
 
-init = tf.compat.v1.global_variables_initializer()
+init = tf.global_variables_initializer()
 
-sess = tf.compat.v1.Session()
+sess = tf.Session()
 sess.run(init)
-train_writer = tf.compat.v1.summary.FileWriter(path_to_store + "/summary", sess.graph)
+train_writer = tf.summary.FileWriter(path_to_store + "/summary", sess.graph)
 train_writer.close()
 
 
 with open(os.path.join(path_to_store, model_name + '.pb'), 'wb') as f:
-    f.write(tf.compat.v1.get_default_graph().as_graph_def().SerializeToString())
+    f.write(tf.get_default_graph().as_graph_def().SerializeToString())
 
 
 
-# builder = tf.compat.v1.saved_model.builder.SavedModelBuilder("C:/Users/Snurka/init_model")
+# builder = tf.saved_model.builder.SavedModelBuilder("C:/Users/Snurka/init_model")
 # builder.add_meta_graph_and_variables(
 #   sess,
-#   [tf.compat.v1.saved_model.tag_constants.SERVING]
+#   [tf.saved_model.tag_constants.SERVING]
 # )
 # builder.save()
 
--- Commons/src/test/resources/tfModelPrototypes/XOR.py
+++ Commons/src/test/resources/tfModelPrototypes/XOR.py
@@ -1,9 +1,9 @@
 import os
 
 import sys
-import tensorflow as tf
+import tensorflow._api.v2.compat.v1 as tf
 
-tf.compat.v1.disable_v2_behavior()
+tf.disable_v2_behavior()
 
 model_name = sys.argv[1]
 input_count = int(sys.argv[2])
@@ -18,48 +18,48 @@ hidden_count_1 = 10
 
 output_count = value_output_count
 
-tf.compat.v1.reset_default_graph()
-tf.compat.v1.random.set_random_seed(seed)
+tf.reset_default_graph()
+tf.random.set_random_seed(seed)
 
-Relu = tf.compat.v1.nn.relu
+Relu = tf.nn.relu
 
-Tanh = tf.compat.v1.nn.tanh
-BatchNormalization = tf.compat.v1.layers.batch_normalization
-Dense = tf.compat.v1.layers.dense
-Dropout = tf.compat.v1.nn.dropout
+Tanh = tf.nn.tanh
+BatchNormalization = tf.layers.batch_normalization
+Dense = tf.layers.dense
+Dropout = tf.nn.dropout
 
-x = tf.compat.v1.placeholder(tf.compat.v1.float64, [None, input_count], name= 'input_node')
-Q_target = tf.compat.v1.placeholder(tf.compat.v1.float64, [None, output_count], name = "target_node")
-keep_prob = tf.compat.v1.placeholder(tf.compat.v1.float64, [], name = "keep_prob_node")
-learning_rate = tf.compat.v1.placeholder(tf.compat.v1.float64, [], name = "learning_rate_node")
+x = tf.placeholder(tf.float64, [None, input_count], name= 'input_node')
+Q_target = tf.placeholder(tf.float64, [None, output_count], name = "target_node")
+keep_prob = tf.placeholder(tf.float64, [], name = "keep_prob_node")
+learning_rate = tf.placeholder(tf.float64, [], name = "learning_rate_node")
 
-hidden_1 = Dense(x,        hidden_count_1, tf.compat.v1.nn.relu, use_bias = True, kernel_initializer = tf.compat.v1.glorot_normal_initializer(), name = "hidden_1")
-hidden_2 = Dense(hidden_1, hidden_count_1, tf.compat.v1.nn.relu, use_bias = True, kernel_initializer = tf.compat.v1.glorot_normal_initializer(), name = "hidden_2")
+hidden_1 = Dense(x,        hidden_count_1, tf.nn.relu, use_bias = True, kernel_initializer = tf.glorot_normal_initializer(), name = "hidden_1")
+hidden_2 = Dense(hidden_1, hidden_count_1, tf.nn.relu, use_bias = True, kernel_initializer = tf.glorot_normal_initializer(), name = "hidden_2")
 
-output = tf.compat.v1.layers.dense(hidden_2, output_count, tf.nn.tanh, use_bias = True, kernel_initializer = tf.compat.v1.zeros_initializer, bias_initializer = tf.compat.v1.zeros_initializer, name = 'output_node')
+output = tf.layers.dense(hidden_2, output_count, tf.nn.tanh, use_bias = True, kernel_initializer = tf.zeros_initializer, bias_initializer = tf.zeros_initializer, name = 'output_node')
 
-prediction = tf.compat.v1.identity(output, name = "prediction_node")
+prediction = tf.identity(output, name = "prediction_node")
 
-loss = tf.compat.v1.keras.losses.mean_squared_error(y_true = Q_target, y_pred = output)
-train_op = tf.compat.v1.train.AdamOptimizer(learning_rate = learning_rate, name = "Optimizer").minimize(loss, name ='optimize_node')
+loss = tf.keras.losses.mean_squared_error(y_true = Q_target, y_pred = output)
+train_op = tf.train.AdamOptimizer(learning_rate = learning_rate, name = "Optimizer").minimize(loss, name ='optimize_node')
 
-init = tf.compat.v1.global_variables_initializer()
+init = tf.global_variables_initializer()
 
-sess = tf.compat.v1.Session()
+sess = tf.Session()
 sess.run(init)
-train_writer = tf.compat.v1.summary.FileWriter(path_to_store + "/summary", sess.graph)
+train_writer = tf.summary.FileWriter(path_to_store + "/summary", sess.graph)
 train_writer.close()
 
 
 with open(os.path.join(path_to_store, model_name + '.pb'), 'wb') as f:
-    f.write(tf.compat.v1.get_default_graph().as_graph_def().SerializeToString())
+    f.write(tf.get_default_graph().as_graph_def().SerializeToString())
 
 
 
-# builder = tf.compat.v1.saved_model.builder.SavedModelBuilder("C:/Users/Snurka/init_model")
+# builder = tf.saved_model.builder.SavedModelBuilder("C:/Users/Snurka/init_model")
 # builder.add_meta_graph_and_variables(
 #   sess,
-#   [tf.compat.v1.saved_model.tag_constants.SERVING]
+#   [tf.saved_model.tag_constants.SERVING]
 # )
 # builder.save()
 
